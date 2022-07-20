f67bff9f73c32d9ba49fcdd283a17029f2b00119
JotaEmePM <perezmjosem@gmail.com>
2022-07-20 15:03:05 -0400

    Corrección prueba Mongo

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { CreateUserDto } from "./dto/create-cat.dto";
 import { Usuario } from "./interfaces/usuario.interface";
 import { UsuarioService } from "./usuario.service";
 
 @Controller('Usuario')
 export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
+
+  @Post()
+  async create(@Body() createCatDto: CreateUserDto) {
+    return this.usuarioService.create(createCatDto);
+  }
 
   @Get()
   async findAll(): Promise<Usuario[]> {
--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,10 +1,13 @@
 import { Module } from "@nestjs/common";
 import { DatabaseModule } from "src/database/database.module";
+import { UsuarioController } from "./usuario.controller";
+import { usuarioProviders } from "./usuario.provider";
+import { UsuarioService } from "./usuario.service";
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [],
-  providers: []
+  controllers: [UsuarioController],
+  providers: [UsuarioService, ...usuarioProviders]
 })
 
 export class UsuarioModule {}
