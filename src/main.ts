7034fb694b66896663c0dece3b775f33670a5010
José Miguel Pérez Méndez <jotaemepm@outlook.com>
2022-07-22 14:01:45 -0400

    Implement Logger

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true })
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  })
 
   // Swagger
   const config = new DocumentBuilder()
