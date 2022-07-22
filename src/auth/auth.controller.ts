87fa571ee17b9711bf2a3b9f9fc203b0ead88ad5
José Miguel Pérez Méndez <jotaemepm@outlook.com>
2022-07-22 13:24:21 -0400

    Eliminado Auth Register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
 import { AuthService } from './auth.service'
-import { RegisterAuthDto } from './dto/register-auth.dto'
+// import { RegisterAuthDto } from './dto/register-auth.dto'
 import { LoginAuthDto } from './dto/login-auth.dto'
 
 @Controller('auth')
@@ -9,10 +9,10 @@ import { LoginAuthDto } from './dto/login-auth.dto'
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('register')
-  registerUser(@Body() userObject: RegisterAuthDto) {
-    console.log({ body: userObject })
-  }
+  // @Post('register')
+  // registerUser(@Body() userObject: RegisterAuthDto) {
+  //   console.log({ body: userObject })
+  // }
 
   @Post('login')
   loginUser(@Body() userObjectLogin: LoginAuthDto) {
